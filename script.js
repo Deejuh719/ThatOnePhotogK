0c088b4c7b5b5bd7fccfe0cc41df5f5aef9e5b26
K Surratt <112124288+Deejuh719@users.noreply.github.com>
2023-10-06 19:23:59 -0400

    ...

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function typing(startIndex){
 function typing2(startIndex){
     var j = startIndex;
     if(j<text2.length){
-        document.getElementById("logDate2").innerHTML += text2.charAt(j);
+        document.getElementsByClassName("h2").innerHTML += text2.charAt(j);
         j++
         setTimeout(function() {
             typing2(j);
